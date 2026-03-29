ae10dadd8141ff45032d5d2ebfea69acae249281
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-03-30 00:33:05 +0500

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,45 @@
+//require('dotenv').config();
+
+import mongoose from 'mongoose'
+import connectDB from './db/index.js'
+import dotenv from 'dotenv'
+
+
+dotenv.config({
+    path:'./env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(async () =>{
+    try {
+     await   mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+     //listner in express
+     app.on("error",(err)=>{
+        console.log("Error in connection")
+        throw err
+     })
+     app.listen(process.env.PORT,()=>{
+        console.log(`App is listening to ${process.env.PORT}`)
+     })
+
+    } catch (error) {
+        console.error("Error",error)
+        throw err
+    }
+})()*/
+
