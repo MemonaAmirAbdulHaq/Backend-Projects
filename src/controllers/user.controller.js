0a6a351dfdfc100a9eb47e075198db23d167673a
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2026-04-06 16:28:48 +0500

    fix: enhance user registration with avatar and cover image handling, and improve error responses

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,8 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
 import {ApiError} from '../utils/ApiError.js';
 import {User} from '../models/user.model.js';
-import {uploadImageToCloudinary, uploadOnCloudinary} from '../utils/cloudinary.js';
+import {uploadOnCloudinary} from '../utils/cloudinary.js';
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 const registerUser=asyncHandler(
     async(req,res)=>{
@@ -39,8 +40,27 @@ const registerUser=asyncHandler(
        throw new ApiError(400,"Avatar image is required")
      }
      const avatar =await uploadOnCloudinary(avatarLocalPath);
-     const coverImage= await uploadOnCloudinary(coverImageLocalPath)
-    }
+     const coverImage= await uploadOnCloudinary(coverImageLocalPath);
+  if(!avatar){
+       throw new ApiError(400,"Avatar image is required")
+
+  }
+
+   const user=await User.create({
+      fullName,
+      avatar:avatar.url,
+      coverImage:coverImage?.url || "",
+      email,
+      password,
+      username:username.toLowerCase()
+     })
+     const createdUser=await User.findById(user._id).select("-password -refreshToken")
+     if(!createdUser){
+      throw new ApiError(500,"Failed to create user")
+     }
+     return res.status(201).json(
+      new ApiResponse(200,createdUser,"User created successfully")
+    )}
 )
 
 export {registerUser}
